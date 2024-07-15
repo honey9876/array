0b5f737cba73033b54276b1801e5fbdfeee80b5a
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-07-15 23:22:50 +0530

    java DSA basic

---
+++ leaner.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class leaner {
+    public static int leaner(int number[], int key){
+        for(int i=0; i<number.length; i++) {
+            if(number[i] == key) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+    public static void main(String[] args) {
+        int number[] = {2, 4, 6, 10, 12, 14, 16};
+        int key = 14;
+
+
+        int index = leaner(number, key);
+        if (index == -1) {
+            System.out.println("not found");
+        }
+        else {
+            System.out.println("key is at index : " + index);
+        }
+        
+    }
+
+
+}
