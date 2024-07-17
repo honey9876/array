5981423a85cdcc54129e3ec89141f190016539bd
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-07-17 21:51:41 +0530

    reverse array

---
+++ reverse.java
@@ -0,0 +1,28 @@
+public class reverse {
+    public static void reverse(int number[]){
+       int first = 0, last = number.length-1;
+
+       while (first < last) {
+          
+        int temp = number[last];
+        number[last] = number[first];
+        number[first] = temp;
+
+        first++;
+        last--;
+
+       }
+    }
+    public static void main(String[] args) {
+        int number[] = {2 , 4 , 6, 8, 10};
+
+        reverse(number);
+        for(int i = 0; i<number.length; i++){
+           System.out.println(number[i]+ " "); 
+        }
+        System.out.println();
+
+        
+    }
+    
+}
