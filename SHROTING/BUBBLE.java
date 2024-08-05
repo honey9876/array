d27a087ea9b609ef69ee30b13d8925df8ae7a032
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-08-05 13:52:21 +0530

    shorted array

---
+++ SHROTING/BUBBLE.java
@@ -0,0 +1,32 @@
+package SHROTING;
+
+public class BUBBLE {
+    public static void bubbleshort(int arr[]) {
+        for(int turn=0; turn<arr.length-1; turn++){
+          for(int j=0; j<arr.length-1-turn; j++){
+            if(arr[j] > arr[j+1] ){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+
+          }  
+        }
+        
+    }
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+"");
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 3, 2, 1};
+        bubbleshort(arr);
+        printArr(arr);
+        
+    }
+    
+}
