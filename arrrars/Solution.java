40b3f7a7691e3a62b6590cbb53002331310839a6
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-08-11 23:39:28 +0530

    errror prob

--- arrrars/Solution.java
+++ arrrars/Solution.java
@@ -1,45 +1,45 @@
-public class Solution {
-    private int n;
-    private int WIDTH;
-    private int[][] t;
+// public class Solution {
+//     private int n;
+//     private int WIDTH;
+//     private int[][] t;
 
-    private int solve(int[][] books, int i, int remainW, int maxHt) {
-        if (i >= n) {
-            return maxHt;
-        }
+//     private int solve(int[][] books, int i, int remainW, int maxHt) {
+//         if (i >= n) {
+//             return maxHt;
+//         }
 
-        if (t[i][remainW] != -1) {
-            return t[i][remainW];
-        }
+//         if (t[i][remainW] != -1) {
+//             return t[i][remainW];
+//         }
 
-        int bookW = books[i][0];
-        int bookH = books[i][1];
+//         int bookW = books[i][0];
+//         int bookH = books[i][1];
 
-        int keep = Integer.MAX_VALUE;
-        int skip = Integer.MAX_VALUE;
+//         int keep = Integer.MAX_VALUE;
+//         int skip = Integer.MAX_VALUE;
 
-        // keep
-        if (bookW <= remainW) { // let's keep it here
-            keep = solve(books, i + 1, remainW - bookW, Math.max(maxHt, bookH));
-        }
+//         // keep
+//         if (bookW <= remainW) { // let's keep it here
+//             keep = solve(books, i + 1, remainW - bookW, Math.max(maxHt, bookH));
+//         }
 
-        // skip and put in next shelf
-        skip = maxHt + solve(books, i + 1, WIDTH - bookW, bookH);
+//         // skip and put in next shelf
+//         skip = maxHt + solve(books, i + 1, WIDTH - bookW, bookH);
 
-        return t[i][remainW] = Math.min(keep, skip);
-    }
+//         return t[i][remainW] = Math.min(keep, skip);
+//     }
 
-    public int minHeightShelves(int[][] books, int shelfWidth) {
-        n = books.length;
-        WIDTH = shelfWidth;
-        t = new int[1001][1001];
-        for (int[] row : t) {
-            Arrays.fill(row, -1);
-        }
+//     public int minHeightShelves(int[][] books, int shelfWidth) {
+//         n = books.length;
+//         WIDTH = shelfWidth;
+//         t = new int[1001][1001];
+//         for (int[] row : t) {
+//             Arrays.fill(row, -1);
+//         }
 
-        int remainW = shelfWidth;
-        return solve(books, 0, remainW, 0);
-    }
-} {
+//         int remainW = shelfWidth;
+//         return solve(books, 0, remainW, 0);
+//     }
+// } {
     
-}
+// }
--- arrrars/trappingw.java
+++ arrrars/trappingw.java
@@ -1,40 +1,40 @@
-import java.util.*;
+// import java.util.*;
 
 
-public class trappingw {
-    public static int trappedRainwater(int height[]) {
-        int n = height.length;
-        int leftmax[] = new int[n];
-        leftmax[0] = height[0];
-        for(int i=1; i<n; i++){
-            leftmax[i] = Math.max(height[i], leftmax[i-1]);        
-        }
+// public class trappingw {
+//     public static int trappedRainwater(int height[]) {
+//         int n = height.length;
+//         int leftmax[] = new int[n];
+//         leftmax[0] = height[0];
+//         for(int i=1; i<n; i++){
+//             leftmax[i] = Math.max(height[i], leftmax[i-1]);        
+//         }
 
-        int rightMax[] = new int[n];
-        rightMax[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--){
-            rightMax[i] = Math.max(height[i], rightMax[i+1]);
-        }
-        int trappedwater = 0;
+//         int rightMax[] = new int[n];
+//         rightMax[n-1] = height[n-1];
+//         for(int i=n-2; i>=0; i--){
+//             rightMax[i] = Math.max(height[i], rightMax[i+1]);
+//         }
+//         int trappedwater = 0;
 
-        for(int i=0; i<n; i++){
-         int waterLevel = Math.min(leftmax[i], rightMax[i]);
-          trappedwater += waterLevel - height[i];
-        }
+//         for(int i=0; i<n; i++){
+//          int waterLevel = Math.min(leftmax[i], rightMax[i]);
+//           trappedwater += waterLevel - height[i];
+//         }
         
 
-        return trappedwater;
+//         return trappedwater;
       
 
 
 
-    public static void main(String[] args) {
-        int height[] = {4, 2, 0, 6, 8, 5 ,7};
-        System.out.println(trappedRainwater(height));
+//     public static void main(String[] args) {
+//         int height[] = {4, 2, 0, 6, 8, 5 ,7};
+//         System.out.println(trappedRainwater(height));
         
-    }
+//     }
     
-}
+// }
 
 
       
