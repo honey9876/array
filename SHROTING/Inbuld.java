5bbed6e915a819b4b9d52e366058952d8274e803
Honey sharma <114321260+honey9876@users.noreply.github.com>
2024-09-03 18:45:23 +0530

    Inbuld function

---
+++ SHROTING/Inbuld.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Inbuld {
+    public static void InsertionSort(Integer arr[]) {
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev >=0 && arr[prev] > curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+        
+    }
+
+
+
+    public static void main(String[] args) {
+        Integer arr[] = {5, 4, 1, 3, 2};
+        Arrays.sort(arr, Collections.reverseOrder());
+        printArr(arr);
+    }
+    
+}
